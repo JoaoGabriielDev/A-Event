acb8bde6749a6f1cf4b7a1eaae337d63b45bf103
JoaoGabriielDev <jgabriellima1067@gmail.com>
2026-03-03 20:09:29 -0300

    Projeto Criado - Primeiro Commit

---
+++ src/main/java/br/com/a_event/AEventApplication.java
@@ -0,0 +1,13 @@
+package br.com.a_event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/a_event/AEventApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.a_event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
